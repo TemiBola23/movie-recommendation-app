5752c270866142a0f8d0945349303f89e1991f71
Kulogun Temitope <122876739+TemiBola23@users.noreply.github.com>
2025-06-29 20:04:04 +0100

    Update movieRoutes.js

--- server/route/movieRoutes.js
+++ server/route/movieRoutes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { discoverMovies } = require('../controllers/movieController');
 
+router.get('/discover', discoverMovies);
+// Optionally add genre fetch, details, etc.
+
+module.exports = router;
