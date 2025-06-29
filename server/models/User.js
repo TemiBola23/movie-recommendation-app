3d56a17ec036742d772cd695a5d862adbbf92352
Kulogun Temitope <122876739+TemiBola23@users.noreply.github.com>
2025-06-29 20:14:00 +0100

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,24 @@
+const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  favorites: [{ type: Number }], // TMDB movie IDs
+  watchlist: [{ type: Number }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
