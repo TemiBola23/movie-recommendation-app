1697f02be41b17bd30341666aec691bdcc4b8323
Kulogun Temitope <122876739+TemiBola23@users.noreply.github.com>
2025-06-29 18:06:10 +0100

    Update movieController.js

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,1 +1,30 @@
 
+const axios = require('axios');
+
+// Existing search, details, genres...
+
+// Discover movies with filters and pagination
+const discoverMovies = async (req, res) => {
+  const { genre, minRating, year, page = 1 } = req.query;
+  const params = {
+    api_key: process.env.TMDB_API_KEY,
+    sort_by: 'popularity.desc',
+    include_adult: false,
+    page,
+  };
+  if (genre) params.with_genres = genre;
+  if (minRating) params['vote_average.gte'] = minRating;
+  if (year) {
+    params['primary_release_date.gte'] = `${year}-01-01`;
+    params['primary_release_date.lte'] = `${year}-12-31`;
+  }
+  try {
+    const resp = await axios.get('https://api.themoviedb.org/3/discover/movie', { params });
+    res.json({ results: resp.data.results, totalPages: resp.data.total_pages });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Discover failed' });
+  }
+};
+
+module.exports = { searchMovies, getMovieDetails, getGenres, discoverMovies };
