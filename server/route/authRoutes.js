1d2012c018c2c4dc9285424cf1742d894b06b09f
Kulogun Temitope <122876739+TemiBola23@users.noreply.github.com>
2025-06-30 22:15:29 +0100

    Update authRoutes.js

--- server/route/authRoutes.js
+++ server/route/authRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express');
+const { register, login } = require('../controllers/authController');
 const router = express.Router();
-const authController = require('../controllers/authController');
-const authMiddleware = require('../middleware/authMiddleware');
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-router.get('/profile', authMiddleware, authController.getProfile);
+router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
