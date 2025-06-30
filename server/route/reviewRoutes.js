f3f2a04561269cc522e26a4e34fb88c94f452ad0
Kulogun Temitope <122876739+TemiBola23@users.noreply.github.com>
2025-06-30 16:01:34 +0100

    Update reviewRoutes.js

--- server/route/reviewRoutes.js
+++ server/route/reviewRoutes.js
@@ -1,17 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {
-  addReview,
-  getReviewsByMovie,
-  getUserReviews
-} = require('../controllers/reviewController');
+const reviewController = require('../controllers/reviewController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-// Public route to get all reviews for a specific movie
-router.get('/movie/:movieId', getReviewsByMovie);
-
-// Authenticated routes
-router.post('/', authMiddleware, addReview);
-router.get('/user', authMiddleware, getUserReviews);
+router.post('/', authMiddleware, reviewController.addReview);
+router.get('/movie/:movieId', reviewController.getMovieReviews);
+router.get('/user', authMiddleware, reviewController.getUserReviews);
 
 module.exports = router;
