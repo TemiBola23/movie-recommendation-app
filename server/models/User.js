7f12cd9cc51330cba05e42039edf924a2bf3d052
Kulogun Temitope <122876739+TemiBola23@users.noreply.github.com>
2025-06-30 16:02:44 +0100

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,37 +1,21 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
-    unique: true,
-    trim: true
+    minlength: 3,
   },
   email: {
     type: String,
     required: true,
     unique: true,
-    lowercase: true
+    lowercase: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
-  favorites: [{ type: Number }], // TMDB movie IDs
-  watchlist: [{ type: Number }]
-});
-
-// Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  next();
-});
-
-// Password validation
-userSchema.methods.comparePassword = async function (enteredPassword) {
-  return bcrypt.compare(enteredPassword, this.password);
-};
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
