481cc7a92ab60af1160ec040e742917b9e170cbb
Kulogun Temitope <122876739+TemiBola23@users.noreply.github.com>
2025-07-02 01:46:22 +0100

    Update movieRoutes.js

--- server/Routes/movieRoutes.js
+++ server/Routes/movieRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const { search, details, recommendations } = require('../controllers/movieController');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
+router.get('/', (req, res) => {
+  res.json({ message: '🎬 Movie route working!' });
+});
 
 router.get('/search', search);                // /api/movies/search?query=&page=
 router.get('/details/:id', details);          // /api/movies/details/:id
