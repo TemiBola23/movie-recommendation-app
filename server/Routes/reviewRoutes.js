9c26b7558e48b202dd197221d60cf368f560e665
Kulogun Temitope <122876739+TemiBola23@users.noreply.github.com>
2025-07-02 19:37:51 +0100

    Delete server directory

--- server/Routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { create, getByMovie } = require('../controllers/reviewController');
-const authMiddleware = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.post('/:id', authMiddleware, create);              // POST /api/reviews/:movieId
-router.get('/movie/:movieId', getByMovie);                // GET /api/reviews/movie/:movieId
-
-module.exports = router;
