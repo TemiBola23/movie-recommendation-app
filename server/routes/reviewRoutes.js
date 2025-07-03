5cc735f661f8fcfb35611395eb717ca7ae278e69
Kulogun Temitope <122876739+TemiBola23@users.noreply.github.com>
2025-07-03 17:48:00 +0100

    Delete server/routes directory

--- server/routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addReview, getReviews } = require('../controllers/reviewController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.post('/:movieId', authMiddleware, addReview);
-router.get('/:movieId', getReviews);
-
-module.exports = router;
