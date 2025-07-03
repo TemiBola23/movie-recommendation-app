bd37f4295cd14ea896a4a55be33409167f27bf5a
Kulogun Temitope <122876739+TemiBola23@users.noreply.github.com>
2025-07-03 17:47:22 +0100

    Delete server/models directory

--- server/models/Review.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const reviewSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  movieId: { type: String, required: true },
-  rating: { type: Number, required: true },
-  comment: { type: String, maxlength: 1000 }
-}, { timestamps: true });
-module.exports = mongoose.model('Review', reviewSchema);
